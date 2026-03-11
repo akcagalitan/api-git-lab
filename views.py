824159ce8460f24f1f5593bb68f5d9da98d7bf1a
akcagalitan <cagalitanandrewken671@gmail.com>
2026-03-12 06:36:46 +0800

    ACTIVITY 1

---
+++ views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Activity
+from .serializers import ActivitySerializer
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    queryset = Activity.objects.all().order_by('-created_at')
+    serializer_class = ActivitySerializer
